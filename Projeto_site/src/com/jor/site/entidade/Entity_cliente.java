72af3e12b4a1b774da987fe3044bcbc2117c8b26
jorliano <jorliano@jorliano-pc>
2015-01-03 18:07:32 -0300

    Continuando a classe cliente

--- Projeto_site/src/com/jor/site/entidade/Entity_cliente.java
+++ Projeto_site/src/com/jor/site/entidade/Entity_cliente.java
@@ -1,5 +1,89 @@
 package com.jor.site.entidade;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Entity_cliente {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	@Column(name= "nome")
+	private String Nome;
+	@Column(name= "cpf")
+	private String Cpf;
+	@Column(name= "endereco")
+	private String Endereco;
+	@Column(name= "cidade")
+	private String Cidade;
+	@Column(name= "telefone")
+	private String Telefone;
+	@Column(name= "email")
+	private String Email;
+	@Column(name= "renda")
+	private double Renda;
+	@Column(name= "data_nas")
+	private Date Data_nas;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getCpf() {
+		return Cpf;
+	}
+	public void setCpf(String cpf) {
+		Cpf = cpf;
+	}
+	public String getEndereco() {
+		return Endereco;
+	}
+	public void setEndereco(String endereco) {
+		Endereco = endereco;
+	}
+	public String getCidade() {
+		return Cidade;
+	}
+	public void setCidade(String cidade) {
+		Cidade = cidade;
+	}
+	public String getTelefone() {
+		return Telefone;
+	}
+	public void setTelefone(String telefone) {
+		Telefone = telefone;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public double getRenda() {
+		return Renda;
+	}
+	public void setRenda(double renda) {
+		Renda = renda;
+	}
+	public Date getData_nas() {
+		return Data_nas;
+	}
+	public void setData_nas(Date data_nas) {
+		Data_nas = data_nas;
+	}
+	
+	
 }
