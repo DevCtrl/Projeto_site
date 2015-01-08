42e2ed4db5468c05175e821936ec1cde5d942b54
jorliano <jorliano@jorliano-pc>
2015-01-08 17:58:10 -0300

    conexão feita com o bando de dados sucesso

--- Projeto_site/src/com/jor/site/entidade/Cliente.java
+++ Projeto_site/src/com/jor/site/entidade/Cliente.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Cliente")
+@Table(name="cliente")
 public class Cliente {
 
 	@Id
--- Projeto_site/src/com/jor/site/util/HibernateUtil.java
+++ Projeto_site/src/com/jor/site/util/HibernateUtil.java
@@ -1,13 +1,26 @@
 package com.jor.site.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-	private static final SessionFactory sessionFactoryBalie = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-
-	public static SessionFactory getSessionFactory() {
-	    return sessionFactoryBalie;
+	private static final SessionFactory sessionFactory;
+	static {
+		try {
+			// Create the SessionFactory from standard (hibernate.cfg.xml)
+			// config file.
+			sessionFactory = new AnnotationConfiguration().configure()
+					.buildSessionFactory();
+			System.out.println("conexão feita com sucesso");
+		} catch (Throwable ex) {
+			// Log the exception.
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
 	}
 
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
 }
