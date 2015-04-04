d04e479a96ed3b6276c9d41378e180fa07cce742
jorliano <jorliano@jorliano-System-Product-Name>
2015-04-04 16:41:34 -0300

    mudanca no site

--- Projeto_site/src/teste.java
+++ Projeto_site/src/teste.java
@@ -7,26 +7,72 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
 import org.hibernate.Session;
 
+import com.jor.site.controle.ClienteControler;
+import com.jor.site.controle.EmpreendedorControler;
+import com.jor.site.controle.ParceriaControler;
+import com.jor.site.controle.ProdutoControler;
 import com.jor.site.controle.RelatioControler;
+import com.jor.site.controle.UsuarioControler;
 import com.jor.site.controle.VendaControler;
+import com.jor.site.entidade.Cliente;
+import com.jor.site.entidade.Empreendedor;
+import com.jor.site.entidade.Parceria;
 import com.jor.site.entidade.Produto;
+import com.jor.site.entidade.Usuario;
 import com.jor.site.util.HibernateUtil;
-
+@ManagedBean(name="teste")
+@ViewScoped
 public class teste {
 	
 	public static void main(String[] args) {  
 	  
-	   List<Produto> p = new ArrayList<Produto>();
+	   
 		 VendaControler comando = new VendaControler();
 	  
        
+	    
+	    
 	  
-	   //p = comando.buscaVenda();
-	 
-	   
-	   System.out.println(p.get(0).getNome()+" teste");
-	}  
+	} 
+	public void sobrecaregar(){
+		for (int i = 101; i < 1000; i++) {
+			Produto p = new Produto();
+			p.setNome("produto"+i);
+			p.setValor_Revenda(2.0);
+			p.setValor_Varejo(3.0);
+			p.setQuantidade(100);
+			p.setDataCadastro("04/04/2015");
+			new ProdutoControler().inserir(p);
+			//cliente
+			Cliente c = new Cliente();
+			c.setNome("cliente"+i);
+			c.setDataCadastro("04/04/2015");
+			c.setEmail("jor@teste.com");
+			new ClienteControler().inserir(c);
+			//Empreendedor
+			Empreendedor emp = new Empreendedor();
+			emp.setNome("Empreendedor"+i);
+			emp.setDataCadastro("04/04/2015");
+			emp.setFlp("45454454");
+			new EmpreendedorControler().inserir(emp);
+			//vendedor
+			Parceria pa = new Parceria();
+			pa.setNome("vendedor"+i);
+			pa.setFone("445555445");
+			new ParceriaControler().inserir(pa);
+			//usuario
+			Usuario u = new Usuario();
+			u.setNome("usuario"+i);
+			u.setLogin("us");
+			u.setSenha("us");
+			u.setPrioridade("master");
+			new UsuarioControler().inserir(u);
+		}
+	}
 	
 }
